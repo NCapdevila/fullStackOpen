088b328a985d34d928dac4502e21095deef38011
Rama <ramirocapdevila23@gmail.com>
2024-11-11 13:47:43 -0300

    ejer 2.2

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -1,10 +1,15 @@
 import Part from './Part'
 const Content = ({content}) =>{
+    const total = content.reduce((acomulador, content)=>{
+        return acomulador + content.exercises 
+    },0)
+    
     return(
         <>
             {content.map(note =>
                 <Part key={note.id} part={note} />
             )}
+        <p>Total: {total}</p>
         </>
     )
 }
